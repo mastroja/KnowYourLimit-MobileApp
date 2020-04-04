d64b16f38e4688cbfc72689adf7347bee2f14c31
Adam Brown <brownadam010@gmail.com>
2020-04-04 11:58:00 -0400

    code Review 2 branch. Added a few classes for retrofit and some test methods as well

---
+++ app/src/main/java/com/example/KnowYouLimit/RetrofitClinetInstance.kt
@@ -0,0 +1,23 @@
+package com.example.KnowYouLimit
+
+//This class creates an instance for you to send an HTTP request to your JSON url and get data back from it.
+/*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClinetInstance {
+    private var retrofit: Retrofit? = null
+    private val BASE_URL = "https://bottomsup-f3e22.firebaseio.com"
+
+    val retrofitInstance : Retrofit?
+        get(){
+            // has this drink been created yet
+            if(retrofit == null){
+            retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+}*/
--- app/src/test/java/com/example/KnowYouLimit/DrinksDataUnitTest.kt
+++ app/src/test/java/com/example/KnowYouLimit/DrinksDataUnitTest.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import com.example.KnowYouLimit.dto.drinks
 import com.example.KnowYouLimit.service.DrinksService
 import com.example.KnowYouLimit.ui.main.MainViewModel
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -31,6 +33,13 @@ class DrinksDataUnitTest {
         givenAFeedOfMockkDrinkDataAreAvailable()
         whenSearchForBudLight()
         thenResultContainsBudLight()
+        thenVerifiedBudLightFunctionsEnvoked()
+    }
+
+    private fun thenVerifiedBudLightFunctionsEnvoked() {
+        verify { drinkService.fetchDrinks("BudLight") }
+        verify(exactly = 0) {drinkService.fetchDrinks("asdfjkl") }
+        confirmVerified(drinkService)
     }
 
     private fun givenAFeedOfMockkDrinkDataAreAvailable() {
@@ -44,14 +53,15 @@ class DrinksDataUnitTest {
 
         //mimmick budlight data as if coming in by JSON
         var budLight = drinks("BudLight", "Beer", "4.9", "12oz")
-
-        //add to array
         allDrinks.add(budLight)
+        var wine = drinks("Cabernet", "Wine", "13", "255ml")
+        allDrinks.add(wine)
 
         //post that array to our mimmicked live data
         allDrinksLiveData.postValue(allDrinks)
 
-        every { drinkService.fetchDrinks(any<String>())} returns allDrinksLiveData
+        every { drinkService.fetchDrinks(or("BudLight", "Wine"))} returns allDrinksLiveData
+        every {drinkService.fetchDrinks(not(or("BudLight", "Wine")))} returns MutableLiveData<ArrayList<drinks>>()
 
         mvm.drinksService = drinkService
     }
@@ -75,6 +85,23 @@ class DrinksDataUnitTest {
         }
 
         assertTrue(budLightFound)
+    }
+
+    @Test
+    fun searchForGarbage_ReturnsNothing(){
+        givenAFeedOfMockkDrinkDataAreAvailable()
+        whenISearchForGarbage()
+        thenIGetZeroResults()
+    }
+
+    private fun whenISearchForGarbage() {
+        mvm.fetchDrinks("asdfjkl")
+    }
+
+    private fun thenIGetZeroResults() {
+        mvm.drinks.observeForever{
+            assertEquals(0, it.size)
+        }
     }
 
 }
